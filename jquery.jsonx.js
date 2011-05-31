f2b6ef0e43664180922aaaa0abdb2b7f03cf0bbb
Alasdair Mercer <mercer.alasdair@gmail.com>
2011-05-31 19:28:40 +0000

    first draft logic and code

--- jquery.jsonx.js
+++ jquery.jsonx.js
@@ -1,7 +1,41 @@
 /**
  * Structure:
  * 
- * 
+ * element
+ *   = '[' tag-name ',' attributes ',' element-list ']'
+ *   | '[' tag-name ',' attributes ']'
+ *   | '[' tag-name ',' element-list ']'
+ *   | '[' tag-name ']'
+ *   | string
+ *   ;
+ * tag-name
+ *   = string
+ *   ;
+ * attributes
+ *   = '{' attribute-list '}'
+ *   | '{' '}'
+ *   ;
+ * attribute-list
+ *   = attribute ',' attribute-list
+ *   | attribute
+ *   ;
+ * attribute
+ *   = attribute-name ':' attribute-value
+ *   ;
+ * attribute-name
+ *   = string
+ *   ;
+ * attribute-value
+ *   = string
+ *   | number
+ *   | 'true'
+ *   | 'false'
+ *   | 'null'
+ *   ;
+ * element-list
+ *   = element ',' element-list
+ *   | element
+ *   ;
  * 
  * Usage:
  * 
@@ -11,29 +45,74 @@
  * (jQuery) $.jsonx('parse', ''|{});
  * (String) $(*).jsonx('stringify');
  * (String) $.jsonx('stringify', {});
+ * @author <a href="http://github.com/neocotic">Alasdair Mercer</a>
  */
 (function ($) {
     var methods = {
         init: function (value) {
-            var j, obj;
+            function convertJQuery(obj) {
+                var ret = [];
+                obj.each(function () {
+                    var $this = $(this),
+                        attrs = {},
+                        contents = $this.contents(),
+                        i;
+                    ret.push(this.nodeName.toLowerCase());
+                    if (this.attributes.length) {
+                        for (i = 0; i < this.attributes.length; i++) {
+                            attrs[this.attributes[i].name] = this.attributes[i].value;
+                        }
+                        ret.push(attrs);
+                    }
+                    if (contents.length) {
+                        for (i = 0; i < contents.length; i++) {
+                            if (contents[i].nodeType === 3) {
+                                ret.push(contents[i].textContent);
+                            } else {
+                                ret.push(convertJQuery($(contents[i])));
+                            }
+                        }
+                    }
+                });
+                return ret;
+            }
             if (typeof value !== 'undefined') {
-                obj = $(value);
-            } else {
-                obj = this;
+                return convertJQuery($(value));
             }
-            if (obj.length) {
-                // TODO: Create JSON(x) from jQuery object
-                j = {};
-            }
-            return j;
+            return convertJQuery(this);
         },
         parse: function (value) {
-            var obj;
+            function convertJsonx(obj, parent) {
+                var arr = [],
+                    ele = {};
+                if ($.isArray(obj)) {
+                    if (!obj.length || typeof obj[0] !== 'string') {
+                        throw new SyntaxError('JSONX.parse');
+                    }
+                    ele = $(document.createElement(obj[0]));
+                    if (obj.length > 1) {
+                        if ($.isPlainObject(obj[1])) {
+                            ele.attr(obj[1]);
+                            if (obj.length > 2) {
+                                arr = Array.prototype.slice.call(obj, 2);
+                            }
+                        } else {
+                            arr = Array.prototype.slice.call(obj, 1);
+                        }
+                        if (arr.length) {
+                            convertJsonx(arr, ele);
+                        }
+                    }
+                } else if (typeof obj === 'string') {
+                    parent.append(obj);
+                }
+                return parent;
+            }
             if (typeof value === 'string') {
                 value = JSON.parse(value);
             }
-            // TODO: Create jQuery object from JSON(x)
-            return obj;
+            // TODO: Should this be children()?
+            return convertJsonx(value, $('<x/>')).contents();
         },
         stringify: function (value) {
             if (typeof value === 'undefined') {
