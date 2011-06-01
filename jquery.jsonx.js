e9a67b4206e117999990494851c8d31f76662afd
Alasdair Mercer <mercer.alasdair@gmail.com>
2011-06-01 14:36:02 +0000

    changed how attributes are read, some logic, and tidied some code

--- jquery.jsonx.js
+++ jquery.jsonx.js
@@ -39,33 +39,44 @@
  * 
  * Usage:
  * 
- * (jQuery) $(*).jsonx(''|[]);
- * (jQuery) $.jsonx(''|[]);
- * (jQuery) $(*).jsonx('build', ''|[]);
- * (jQuery) $.jsonx('build', ''|[]);
- * (Array) $(*).jsonx('parse', ''|jQuery);
- * (Array) $.jsonx('parse', ''|jQuery);
- * (String) $(*).jsonx('stringify', []|jQuery);
- * (String) $.jsonx('stringify', []|jQuery);
+ * (jQuery) $(*).jsonx(String|Array);
+ * (jQuery) $.jsonx(String|Array);
+ * (jQuery) $(*).jsonx('build', String|Array);
+ * (jQuery) $.jsonx('build', String|Array);
+ * (Array) $(*).jsonx('parse', String|jQuery);
+ * (Array) $.jsonx('parse', String|jQuery);
+ * (String) $(*).jsonx('stringify', Array|jQuery);
+ * (String) $.jsonx('stringify', Array|jQuery);
  * 
  * @author <a href="http://github.com/neocotic">Alasdair Mercer</a>
  * @requires jQuery
  */
-(function ($) {
+(function ($, undefined) {
+    var helper = {
+        attributes: function (obj) {
+            var ele = obj[0], i = 0, name, notXml = ele.nodeType !== 1 || !$.isXMLDoc(ele), ret = {};
+            if (ele.attributes.length > 0) {
+                for (; i < ele.attributes.length; i++) {
+                    name = ele.attributes[i].name;
+                    name = notXml && $.propFix[name] || name;
+                    map[name] = obj.attr(name);
+                }
+            }
+            return ret;
+        }
+    };
     var api = {
         /**
          * <p></p>
-         * @param {String|Array} value The JSONX string or array to be
-         * transformed in to a jQuery object.
-         * @returns {jQuery} The generated jQuery object.
+         * @param {String|Array} value
+         * @returns {jQuery}
          */
         build: function (value) {
             function convertJsonx(obj, parent) {
-                var arr = [],
-                    ele = {};
+                var arr = [], ele = {};
                 if ($.isArray(obj)) {
-                    if (!obj.length || typeof obj[0] !== 'string') {
-                        $.error('jsonx.build: Syntax error');
+                    if (!obj.length || $.type(obj[0]) !== 'string') {
+                        $.error('JSONX.build: Syntax error');
                     }
                     ele = $(document.createElement(obj[0]));
                     if (obj.length > 1) {
@@ -81,69 +92,69 @@
                             convertJsonx(arr, ele);
                         }
                     }
-                } else if (typeof obj === 'string') {
+                } else if ($.type(obj) === 'string') {
                     parent.append(obj);
                 }
                 return parent;
             }
-            if (typeof value === 'string') {
+            if ($.type(value) === 'string') {
                 value = api.parse(value);
+            }
+            if (!$.isArray(value)) {
+                $.error('JSONX.build: Type error');
             }
             return convertJsonx(value, $('<x/>')).contents();
         },
         /**
          * <p></p>
-         * @param {String|jQuery} [value] The JSONX string to be parsed or the
-         * jQuery object to be transformed in to a JSONX array. If omitted, an
-         * attempt will be made to transform the current jQuery object.
-         * @returns {Array} The generated JSONX array.
+         * @param {String|jQuery} [value]
+         * @returns {Array}
          */
         parse: function (value) {
             function convertJQuery(obj) {
                 var ret = [];
                 obj.each(function () {
-                    var $this = $(this),
-                        attrs = {},
-                        contents = $this.contents(),
-                        i;
-                    ret.push(this.nodeName.toLowerCase());
-                    // TODO: Ensure logic is sound and no mapping is required (e.g. for keywords)
-                    if (this.attributes.length) {
-                        for (i = 0; i < this.attributes.length; i++) {
-                            attrs[this.attributes[i].name] = this.attributes[i].value;
-                        }
-                        ret.push(attrs);
+                    var $this = $(this), attrs = {}, contents = $this.contents(), i = 0;
+                    Array.prototype.push.call(ret, this.nodeName.toLowerCase());
+                    attrs = helper.attributes($this);
+                    if (!$.isEmptyObject(attrs)) {
+                        Array.prototype.push.call(ret, attrs);
                     }
                     if (contents.length) {
-                        for (i = 0; i < contents.length; i++) {
+                        for (; i < contents.length; i++) {
                             if (contents[i].nodeType === 3) {
-                                ret.push(contents[i].textContent);
+                                Array.prototype.push.call(ret, contents[i].textContent);
                             } else {
-                                ret.push(convertJQuery($(contents[i])));
+                                Array.prototype.push.call(ret, convertJQuery($(contents[i])));
                             }
                         }
                     }
                 });
                 return ret;
             }
-            if (typeof value === 'string') {
-                return JSON.parse(value);
+            if (value === undefined) {
+                value = this;
             }
-            value = value || this;
-            return convertJQuery($(value));
+            switch ($.type(value)) {
+                case 'string':
+                    return JSON.parse(value);
+                case 'object':
+                    return convertJQuery($(value));
+            }
+            $.error('JSONX.parse: Type error');
         },
         /**
          * <p></p>
-         * @param {Array|jQuery} [value] The JSONX array or jQuery object to be
-         * transformed in to a JSONX string. If omitted, an attempt will be made
-         * to transform the current jQuery object.
-         * @return {String} The generated JSONX string.
+         * @param {Array|jQuery} [value]
+         * @return {String}
          */
         stringify: function (value) {
-            if (typeof value === 'undefined') {
+            if (value === undefined) {
                 value = api.parse(this);
-            } else if (typeof value.jquery === 'string') {
+            } else if ($.type(value) === 'object' && value.jquery) {
                 value = api.parse(value);
+            } else if (!$.isArray(value)) {
+                $.error('JSONX.stringify: Type error');
             }
             return JSON.stringify(value);
         }
